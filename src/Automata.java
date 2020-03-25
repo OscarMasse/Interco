9623e73512b26862cbef47413745b640f8259eaa
Syned <oscar@masse.pro>
2020-03-25 12:01:57 +0100

    Adding Automata.java and Multiplex.java

---
+++ src/Automata.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Automata {
+
+    private List<String> var;
+
+    public Automata(List<String> var) {
+        this.var = var;
+    }
+}
--- src/Reader.java
+++ src/Reader.java
@@ -31,7 +31,6 @@ public class Reader {
                 readPARA(smb,line);
             }
         }
-
     }
 
     private void  readVAR(BufferedReader smb, String line){
